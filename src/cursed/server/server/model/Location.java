eb5497d65b62fa6468ed901b98bc080e33c69af1
ibmboy19 <ibmboy19@gmail.com>
2012-05-07 16:50:43 +0000

    1.補上map與Location

---
+++ src/cursed/server/server/model/Location.java
@@ -0,0 +1,8 @@
+package cursed.server.server.model;
+
+public class Location {
+	public Location(Location loc) {
+		//this(loc._x, loc._y, loc._map);
+	}
+
+}
---
+++ src/cursed/server/server/model/map/map.java
@@ -0,0 +1,5 @@
+package cursed.server.server.model.map;
+
+public class map {
+
+}
