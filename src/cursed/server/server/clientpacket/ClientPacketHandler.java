4a86e5fd4d7d177dcec589d715233bfa7d5822da
boneache <boneache@gmail.com>
2012-11-14 07:49:57 +0000

    角色創建insert to database 可以了

--- src/cursed/server/server/clientpacket/ClientPacketHandler.java
+++ src/cursed/server/server/clientpacket/ClientPacketHandler.java
@@ -1,17 +1,6 @@
 package cursed.server.server.clientpacket;
 
-import static cursed.server.server.clientpacket.ClientOpcodes.C_ChangeModel;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_ChangeTexture;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_Chat;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_CreateCharacter;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_KeyBoardWalk;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_Logout;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_Party;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_PartyApply;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_RequestCharacterList;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_RequestInventory;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_Walk;
-import static cursed.server.server.clientpacket.ClientOpcodes.C_login;
+import static cursed.server.server.clientpacket.ClientOpcodes.*;
 
 import java.io.IOException;
 import java.net.SocketException;
@@ -36,7 +25,7 @@ public class ClientPacketHandler {
 		
 		try {
 			switch (op) {
-			case C_login:
+			case C_Login:
 				String accountName = _client.getBr().readLine();
 				String password = _client.getBr().readLine();
 				String ip = _client.get_ip();
@@ -53,7 +42,7 @@ public class ClientPacketHandler {
 					pc.setAccountName(accountName);
 				}
 				if (!account.validatePassword(password)) {
-					_client.getWr().println(C_login);
+					_client.getWr().println(C_Login);
 					_client.getWr().println("false");
 					return;
 				}
@@ -62,7 +51,7 @@ public class ClientPacketHandler {
 					LoginController.getInstance().login(_client, account);
 					//Account.updateLastActive(account, ip); // 更新最後一次登入的時間與IP
 					_client.setAccount(account);
-					_client.getWr().println(C_login);
+					_client.getWr().println(C_Login);
 					_client.getWr().println("true");
 					
 					System.out.format("帳號: %s 已經登入\n", accountName);					
@@ -92,7 +81,8 @@ public class ClientPacketHandler {
 				//若client 收到true，將會送一次OP_RequestCharacterList；false則不做事
 				break;
 			case C_RequestCharacterList:
-				//client要求該帳號的角色清單
+				//TODO client要求該帳號的角色清單
+				//回傳給client角色的清單 op, character_id , character_lv,guild_name
 			break;
 			case C_Chat:
 				String msg = null;
